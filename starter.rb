725a1afe53cee2a68dabe9bfa92b725c50c0100b
yyy <y.w.k.w.1717@gmail.com>
2018-01-27 16:27:22 +0900

    [Fix] typo

--- starter.rb
+++ starter.rb
@@ -2,7 +2,7 @@ require_relative 'lib/ms17_010_scan'
 require_relative 'lib/double_pulsar_scan'
 
 # MS17-010 check
-@ms17_010_scan = Ms17010Scan.new("10.10.10.10")
+@ms17_010_scan = Ms17010Scan.new('10.10.10.10')
 @ms17_010_scan.start
 
 # DoublePulsar check
