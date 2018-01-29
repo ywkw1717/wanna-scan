3679cf14b7d6ca745839f427c272a655ddf74b58
yyy <y.w.k.w.1717@gmail.com>
2018-01-29 19:51:25 +0900

    [Update] ip_list.rb

--- lib/ip_list.rb
+++ lib/ip_list.rb
@@ -1,5 +1,14 @@
 class IPList
   def initialize
-    ip_list = `arp -a -n |grep -o -e "[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+"`
+    ip_command = `arp -a -n`
+    @ip_list    = []
+
+    ip_command.each_line do |s|
+      @ip_list.push(s.slice(/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/))
+    end
+  end
+
+  def ip_list
+    @ip_list
   end
 end
