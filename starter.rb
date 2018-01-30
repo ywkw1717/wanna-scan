c56d60de9d7ad2facfd9c4109b0558d308675d3d
yyy <y.w.k.w.1717@gmail.com>
2018-01-30 20:59:43 +0900

    [Add] option

--- starter.rb
+++ starter.rb
@@ -1,9 +1,46 @@
+require 'optparse'
 require_relative 'lib/ms17_010_scan'
 require_relative 'lib/double_pulsar_scan'
 require_relative 'lib/host_scan.rb'
 require_relative 'lib/port_scan.rb'
 
-host_scan = HostScan.new
+def scan_ip(ip)
+  ms17_010_scan = Ms17010Scan.new(ip)
+  ms17_010_scan.start
+  double_pulsar_scan = DoublePulsarScan.new(ip)
+  double_pulsar_scan.start
+end
+
+def usage
+  puts <<-"EOS"
+Usage: ruby starter.rb [options] {value}
+
+Options:
+   i [ip addr]  scan ip
+  -I [nic]      scan nic
+EOS
+end
+
+params = ARGV.getopts('i:I:')
+
+if params["i"] && params["I"]
+  puts "You can not specify both options.\n\n"
+  usage
+  return
+elsif !params["i"] && !params["I"]
+  usage
+  return
+end
+
+# [TODO] check the input value?
+ip = params["i"]
+unless ip.nil?
+  scan_ip(ip)
+  return
+end
+
+# [TODO] check the input value?
+host_scan = HostScan.new(params["I"])
 port_scan = PortScan.new
 
 # Search host opend 445 port
@@ -11,12 +48,8 @@ threads = []
 host_scan.ip_list.each { |s| threads << Thread.new { port_scan.start(s) } }
 threads.each(&:join)
 
+# [TODO] output result in a file
 # MS17-010 and DoublePulsar check
 threads = []
-port_scan.open_445_list.each { |ip| threads << Thread.new {
-  ms17_010_scan = Ms17010Scan.new(ip)
-  ms17_010_scan.start
-  double_pulsar_scan = DoublePulsarScan.new(ip)
-  double_pulsar_scan.start
-} }
+port_scan.open_445_list.each { |ip| threads << Thread.new { scan_ip(ip) } }
 threads.each(&:join)
