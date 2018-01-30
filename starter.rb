df1626992ed10138faf8f42cd9520ae3cc6232a9
yyy <y.w.k.w.1717@gmail.com>
2018-01-30 18:38:26 +0900

    [Add] thread

--- starter.rb
+++ starter.rb
@@ -1,10 +1,22 @@
 require_relative 'lib/ms17_010_scan'
 require_relative 'lib/double_pulsar_scan'
+require_relative 'lib/host_scan.rb'
+require_relative 'lib/port_scan.rb'
 
-# MS17-010 check
-@ms17_010_scan = Ms17010Scan.new('10.10.10.10')
-@ms17_010_scan.start
+host_scan = HostScan.new
+port_scan = PortScan.new
 
-# DoublePulsar check
-@double_pulsar_scan = DoublePulsarScan.new('10.10.10.10')
-@double_pulsar_scan.start
+# Search host opend 445 port
+threads = []
+host_scan.ip_list.each { |s| threads << Thread.new { port_scan.start(s) } }
+threads.each(&:join)
+
+# MS17-010 and DoublePulsar check
+threads = []
+port_scan.open_445_list.each { |ip| threads << Thread.new {
+  ms17_010_scan = Ms17010Scan.new(ip)
+  ms17_010_scan.start
+  double_pulsar_scan = DoublePulsarScan.new(ip)
+  double_pulsar_scan.start
+} }
+threads.each(&:join)
