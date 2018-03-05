193c391aeac3bbf141daf03bd4d0e6e3222f0751
yyy <y.w.k.w.1717@gmail.com>
2018-03-05 17:34:04 +0900

    [Add] output results in a file

--- wanna_scan.rb
+++ wanna_scan.rb
@@ -1,4 +1,5 @@
 require 'optparse'
+require 'pathname'
 require_relative 'lib/ms17_010_scan'
 require_relative 'lib/double_pulsar_scan'
 require_relative 'lib/host_scan.rb'
@@ -9,14 +10,31 @@ def usage
     Usage: ruby wanna_scan.rb [options] {value}
 
     Options:
-      -i [ip addr]  scan ip
-      -I [nic]      scan nic
+      -i [ip addr]           scan ip
+      -I [interface]         scan interface
+      -o [output directory]  output in the directory
   USAGE
 end
 
-params = ARGV.getopts('i:I:')
+def output(dir)
+  return if dir.nil?
+
+  date = Time.new.strftime('%Y%m%d-%H%M%S')
+  path = Pathname.new(dir).join('wanna_scan_' + date + '.txt')
+
+  File.open(path, 'w') do |f|
+    f.puts(date)
+    f.puts("[+] Vulnerability of MS17-010 list\n")
+    @ms17_010_scan.vulnerable_host.each { |s| f.puts(s) }
+
+    f.puts("\n[+] Infected with DoublePulsar list")
+    @double_pulsar_scan.vulnerable_host.each { |s| f.puts(s) }
+  end
+end
+
+params = ARGV.getopts('i:I:o:')
 if params['i'] && params['I']
-  puts "You can not specify both options.\n\n"
+  puts "You can not specify many options.\n\n"
   puts usage
   exit
 elsif !params['i'] && !params['I']
@@ -37,30 +55,31 @@ unless ip.nil?
 
   puts('[*] MS17-010 Scan start')
 
-  ms17_010_scan = Ms17010Scan.new
-  ms17_010_scan.start(ip)
+  @ms17_010_scan = Ms17010Scan.new
+  @ms17_010_scan.start(ip)
 
-  if ms17_010_scan.vulnerable_host.empty?
+  if @ms17_010_scan.vulnerable_host.empty?
     puts '[-] The vulnerability is not found'
   else
-    puts("[+] #{ms17_010_scan.vulnerable_host[0]} has a vulnerability of MS17-010")
+    puts("[+] #{@ms17_010_scan.vulnerable_host[0]} has a vulnerability of MS17-010")
   end
 
   puts('[*] MS17-010 Scan finish')
 
   puts("\n[*] DoublePulsar Scan start")
 
-  double_pulsar_scan = DoublePulsarScan.new
-  double_pulsar_scan.start(ip)
+  @double_pulsar_scan = DoublePulsarScan.new
+  @double_pulsar_scan.start(ip)
 
-  if double_pulsar_scan.vulnerable_host.empty?
+  if @double_pulsar_scan.vulnerable_host.empty?
     puts('[-] DoublePulsar is not found')
   else
-    puts "[+] #{double_pulsar_scan.vulnerable_host[0]} has been infected with DoublePulsar"
+    puts "[+] #{@double_pulsar_scan.vulnerable_host[0]} has been infected with DoublePulsar"
   end
 
   puts("[*] DoublePulsar Scan finish\n\n")
 
+  output(params['o'])
   exit
 end
 
@@ -77,41 +96,42 @@ host_scan.ip_list.each do |s|
 end
 threads.each(&:join)
 
-# [TODO] output result in a file
 # MS17-010 scan
 puts('[*] MS17-010 Scan start')
-ms17_010_scan = Ms17010Scan.new
+@ms17_010_scan = Ms17010Scan.new
 
 port_scan.open_445_list.each do |host|
   threads << Thread.new do
-    ms17_010_scan.start(host)
+    @ms17_010_scan.start(host)
   end
 end
 threads.each(&:join)
 
 puts("[+] Vulnerability of MS17-010 list\n")
-if ms17_010_scan.vulnerable_host.empty?
+if @ms17_010_scan.vulnerable_host.empty?
   puts('nothing')
 else
-  puts ms17_010_scan.vulnerable_host
+  puts @ms17_010_scan.vulnerable_host
 end
 puts('[*] MS17-010 Scan finish')
 
 # DoublePulsar scan
 puts("\n[*] DoublePulsar Scan start")
-double_pulsar_scan = DoublePulsarScan.new
+@double_pulsar_scan = DoublePulsarScan.new
 
 port_scan.open_445_list.each do |host|
   threads << Thread.new do
-    double_pulsar_scan.start(host)
+    @double_pulsar_scan.start(host)
   end
 end
 threads.each(&:join)
 
 puts('[+] Infected with DoublePulsar list')
-if double_pulsar_scan.vulnerable_host.empty?
+if @double_pulsar_scan.vulnerable_host.empty?
   puts('nothing')
 else
-  puts double_pulsar_scan.vulnerable_host
+  puts @double_pulsar_scan.vulnerable_host
 end
 puts("[*] DoublePulsar Scan finish\n\n")
+
+output(params['o'])
